8b676a1c9d649e58a3e6f2771e015a937e922eb3
Fernando Moraes <Fernando@Fedaykin.Home>
2020-11-17 16:57:20 -0300

    Solution to the proposed exercise Clock in Exercism.io

---
+++ clock.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const TO_MILISECONDS = 60000;// 60 (quantite of seconds in a minute) * 1000 (to get in the miliseconds)
+
+export class Clock {
+  constructor(hour, minutes = 0) {
+   this.clock = new Date((hour * 60 + minutes) * TO_MILISECONDS)  
+  }
+
+  toString() {
+    return this.clock.toUTCString().slice(17,22);
+  }
+
+  plus(minutes) {
+    this.clock = new Date(this.clock.getTime() + (minutes * TO_MILISECONDS));
+    return this;
+  }
+
+  minus(minutes) {
+    this.clock = new Date(this.clock.getTime() - (minutes * TO_MILISECONDS));
+    return this;
+  }
+
+  equals(clock) {
+   return this.toString() == clock.toString();
+  }
+}
